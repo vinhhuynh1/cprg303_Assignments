ab4439ae0fda4586a0926ce9173539843dd9723c
vinhhuynh1 <vinhhuynh173@gmail.com>
2023-11-19 16:25:11 -0700

    Added Lab2 Layout Components

---
+++ App.jsx
@@ -0,0 +1,31 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+        <ToDoList/ >
+        <ToDoForm/ >
+    </SafeAreaView>
+  );
+}
+
+export default App;
